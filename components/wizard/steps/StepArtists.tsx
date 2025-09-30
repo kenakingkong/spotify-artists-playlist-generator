fab83ed9525edc7330184b0fc035c0faa5c1654a
Makena Kong <kena.kong@gmail.com>
2025-09-29 18:41:10 -0700

    forgot - im going tto set up some querying and storage system

--- components/wizard/steps/StepArtists.tsx
+++ components/wizard/steps/StepArtists.tsx
@@ -1,5 +1,38 @@
+import { useEffect } from "react";
 import WizardLayout from "../WizardLayout";
+import axios from "axios";
+import { useAuth } from "@/contexts/AuthContext";
 
 export default function StepArtists() {
-  return <WizardLayout>Artists</WizardLayout>;
+  const { user } = useAuth();
+
+  useEffect(() => {
+    let isMounted = true;
+
+    async function fetchTopArtists() {
+      try {
+        const response = await axios.get("/api/spotify/me/top-artists?limit=5");
+        if (!isMounted) return;
+
+        console.log(response.data);
+      } catch (err) {
+        if (!isMounted) return;
+
+        console.error(err);
+      }
+    }
+
+    // if (user) fetchTopArtists();
+
+    return () => {
+      isMounted = false;
+    };
+  }, [user]);
+
+  return (
+    <WizardLayout>
+      <p>Artists</p>
+      <p>Your top artists</p>
+    </WizardLayout>
+  );
 }
