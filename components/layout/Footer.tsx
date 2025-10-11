16944736b3e11133c8b9cc17c856142c629fbde6
Makena Kong <kena.kong@gmail.com>
2025-10-11 03:39:21 -0700

    added site to footer - references #10

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,3 +1,17 @@
 export default function Footer() {
-  return <nav>footer</nav>;
+  return (
+    <nav>
+      <p className="text-xs">
+        Made by{" "}
+        <a
+          href="https://makenakong.com"
+          target="_blank"
+          className="underline hover:text-green-600"
+        >
+          Makena Kong
+        </a>{" "}
+        👩🏻‍💻
+      </p>
+    </nav>
+  );
 }
